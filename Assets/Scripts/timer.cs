93bf77182036305328d991e440cb3b7d4070786c
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-27 21:59:23 +0100

    It works (for now).
    When the timer goes to 0, the player and the enemies now destroy themselves.

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -6,6 +6,7 @@ public class timer : MonoBehaviour
 {
     [SerializeField] float gameTimer = 100;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject enemies;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +19,16 @@ public class timer : MonoBehaviour
         if (gameTimer <= 0)
         {
             Destroy(player);
+            var enem = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemies in enem)
+            {
+                Destroy(enemies);
+            }
         }
     }
     public float GetgameTimer()
     {
         return gameTimer;
+
     }
 }
