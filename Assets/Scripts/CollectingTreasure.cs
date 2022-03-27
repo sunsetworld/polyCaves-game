6647b8a6462041f5007d7fccc9acde14ce2f09ab
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-27 10:42:09 +0100

    Added multiplier, collectibles and other parameters.

---
+++ Assets/Scripts/CollectingTreasure.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectingTreasure : MonoBehaviour
+{
+    [SerializeField] int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(score.ToString());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Treasure")
+        {
+            Destroy(other.gameObject);
+            score += 1;
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
--- Assets/Scripts/gameConfig.cs
+++ Assets/Scripts/gameConfig.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class gameConfig : MonoBehaviour
 {
+    [SerializeField] int scoreMultiplier = 1;
+    // The multiplier of score as definied by settings for lives, playerSpeed and enemySpeed. 
+    [SerializeField] int lives = 1;
+    // If the player chooses more lives, the multiplier will decrease.
+    [SerializeField] int playerSpeed = 1;
+    // If the player chooses a faster player, the multiplier will decrease.
+    [SerializeField] int enemySpeed = 1;
+    // If the player chooses faster enemies, the multiplier will increase. 
+
     // Start is called before the first frame update
     void Start()
     {
