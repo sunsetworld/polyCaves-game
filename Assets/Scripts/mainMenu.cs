91f6f63ab6dc9887998f447e67a85ce8e4b27e56
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-28 16:30:20 +0100

    Working build.
    I added a main menu and a game over to the game.

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    [SerializeField] Canvas mainMenuCanvas;
+    [SerializeField] Canvas settingsCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openSettings()
+    {
+        mainMenuCanvas.enabled = false;
+        settingsCanvas.enabled = true;
+    }
+
+    public void closeSettings()
+    {
+        settingsCanvas.enabled = false;
+        mainMenuCanvas.enabled = true;
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
