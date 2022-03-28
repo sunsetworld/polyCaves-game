a3331453f704e030f949ad2cb253eb264eff0c81
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-03-28 09:58:00 +0100

    Maybe?

--- Assets/Scripts/CollectingTreasure.cs
+++ Assets/Scripts/CollectingTreasure.cs
@@ -29,4 +29,15 @@ public class CollectingTreasure : MonoBehaviour
     {
         return score;
     }
+<<<<<<< HEAD
+=======
+
+    private void loadNextLevel()
+    {
+        if (treasure.Count == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+>>>>>>> parent of 8acc9fa (:()
 }
