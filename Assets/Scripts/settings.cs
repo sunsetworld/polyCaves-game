565ef91ea791cd4d16eaca00228689be27a7c667
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-04-13 11:02:50 +0100

    Implemented Volume change.

--- Assets/Scripts/settings.cs
+++ Assets/Scripts/settings.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public AudioMixer myAM;
+    public void SetVolume(float vol)
     {
-        
+        myAM.SetFloat("Volume", vol);
     }
 }
